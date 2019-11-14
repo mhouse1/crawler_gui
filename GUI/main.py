51649ff309df547b2832552aebe8131ee8d94827
Michael <patzer9@gmail.com>
2019-11-14 03:46:18 -0600

    updated GUI emergency stop images on click, speed slider changes now has call back function, link widgets with function calls

--- GUI/main.py
+++ GUI/main.py
@@ -1,13 +1,9 @@
 '''
-Created on Aug 13, 2014
+Created on Nov. 13, 2019
 
 @author: Mike
 
-@details    GUI interface allowing user to:
-                configure pulse width for each axis
-                jog the X, Y, Z axis or jog XY axis at the same time
-                transmit parsed gcode to firmware for routing, start
-                pause and clear route planned.
+@details    crawler GUI
 '''
 import gtk
 import time
@@ -77,32 +73,19 @@ class CrawlerGUI(GuiSupport):
         self.encoder4 = self.builder.get_object('label38')
 
         #toggle buttons
-        self.toggleAutonomous = self.builder.get_object('togglebutton9')
-        self.toggleSemiAuto = self.builder.get_object('togglebutton10')
-        self.toggleManual = self.builder.get_object('togglebutton11')
-        
-        #text box objects
-        self.StepNumZ       = self.builder.get_object('StepNumZ')
-        self.StepNumY       = self.builder.get_object('StepNumY')
-        self.StepNumX       = self.builder.get_object('StepNumX')
-        self.pulsewidth_z_h = self.builder.get_object('pulsewidth_z_h')
-        self.pulsewidth_z_l = self.builder.get_object('pulsewidth_z_l')
-        self.pulsewidth_y_h = self.builder.get_object('pulsewidth_y_h')
-        self.pulsewidth_y_l = self.builder.get_object('pulsewidth_y_l')
-        self.pulsewidth_x_h = self.builder.get_object('pulsewidth_x_h')
-        self.pulsewidth_x_l = self.builder.get_object('pulsewidth_x_l')
-        self.feed_cut       = self.builder.get_object('feed_cut')
-        self.gcode_scale    = self.builder.get_object('gcode_scale')
-        self.layer_thickness= self.builder.get_object('layer_thickness')
-        self.layer_numbers  = self.builder.get_object('layer_numbers')
-        self.speed_start    = self.builder.get_object('speed_start')
-        self.speed_change   = self.builder.get_object('speed_change')                                                
+        self.crawlDirection = self.builder.get_object('togglebutton9')
+        self.disableBalancing = self.builder.get_object('togglebutton11')
+
+        self.emergencyStopImage = self.builder.get_object('image7')
+        self.emergencyToggle = False
+                                               
 
         #slider, aka scale objects
         self.inclineLeftRight       = self.builder.get_object('hscale1')
         self.inclineFrontBack       = self.builder.get_object('vscale1')
         self.movementLeftRight      = self.builder.get_object('hscale2')
         self.movementReverseForward = self.builder.get_object('hscale3')
+        self.crawlerSpeed          = self.builder.get_object('speedScale')                        
         
         self.inclineLeftRight.set_digits(2)#sets number of display percisions
         #self.inclineLeftRight.set_has_origin(True)#this does not work
@@ -135,201 +118,48 @@ class CrawlerGUI(GuiSupport):
         self.data_update()
 
     ###################### Actions for all signals#########################
-    def on_togglebutton9_toggled(self,widget):
-        autonomous = self.toggleAutonomous.get_active()
-        semiauto = self.toggleSemiAuto.get_active()
-        manual = self.toggleManual.get_active()
-        if autonomous == True:
-            self.toggleManual.set_active(False)
-            self.toggleSemiAuto.set_active(False)
-        
-    def on_togglebutton10_toggled(self,widget):
-        autonomous = self.toggleAutonomous.get_active()
-        semiauto = self.toggleSemiAuto.get_active()
-        manual = self.toggleManual.get_active()
-        if semiauto == True:
-            self.toggleAutonomous.set_active(False)
-            self.toggleManual.set_active(False)
+    def on_speedScale_value_changed(self,a):
+        '''
+        set percentage of speed
+        '''
+        print 'speed event'
 
+
+    def on_togglebutton9_clicked(self,widget):
+        '''
+        forward
+        '''
+        status = widget.get_active()
+
+        if not status:
+            widget.set_label('REVERSE')
+            print 'send direction command',status
+        else:
+            widget.set_label('FORWARD')
+            print 'send direction command',status
+    
+    def on_eventbox1_button_press_event(self,a,b):
+        print 'eventbox pressed'
+        self.emergencyToggle = not self.emergencyToggle
+        if self.emergencyToggle:
+            self.emergencyStopImage.set_from_file(r'C:\Users\Mech1\Documents\crawler_gui\GUI\emergency2.jpg')
+        else:
+            self.emergencyStopImage.set_from_file(r'C:\Users\Mech1\Documents\crawler_gui\GUI\emergency1.png')
+
+
+    def on_image7_button_press_event(self,widget):
+
+        print 'Send stop command'
 
     def on_togglebutton11_toggled(self,widget):
-        autonomous = self.toggleAutonomous.get_active()
-        semiauto = self.toggleSemiAuto.get_active()
-        manual = self.toggleManual.get_active()
-        if manual == True:
-            self.toggleAutonomous.set_active(False)
-            self.toggleSemiAuto.set_active(False)
-        
-    def on_reverse_x_toggled(self,widget, data = None):
-        '''
-        if reversed check box is toggled,
-        use software to reverse axis direction of movement
-        note: this just reverses the direction being sent
-              it does not toggle the firmware reverse bit.
-        '''
-        self.axis_x.dir_reversed = widget.get_active()
-        
-    def on_reverse_y_toggled(self,widget, data = None):
-        '''
-        if reversed check box is toggled,
-        use software to reverse axis direction of movement
-        note: this just reverses the direction being sent
-              it does not toggle the firmware reverse bit.
-        '''
-        self.axis_y.dir_reversed = widget.get_active()
-
-    def on_reverse_z_toggled(self,widget, data = None):
-        '''
-        if reversed check box is toggled,
-        use software to reverse axis direction of movement
-        note: this just reverses the direction being sent
-              it does not toggle the firmware reverse bit.
-        '''
-        self.axis_z.dir_reversed = widget.get_active()
-        
-    def on_servo_clicked(self, widget, data = None):
-        '''
-        to be used in the future to control router RPM
-        '''
-        self.SendSinleCFData.send(self.builder.get_object('servo_value'))
-        
-    def on_tst_gr1_button1_clicked(self,widget,data=None):
-        '''
-        dummy button: use this to run whatever code you wish
-        '''
-        #print how many items are left in slow_queue
-        print Communications.slow_queue.qsize(),' in slow queue'
-    
-    def on_set_acceleration_clicked(self,widget):
-        '''
-        sets the acceleration profile for all axis
-        '''
-        self.gs_speed_start = int(self.speed_start.get_text()) 
-        self.gs_speed_change = int(self.speed_change.get_text()) 
-        self.set_acceleration()    
-            
-    def on_jog_xy_clicked(self,widget):
-        '''
-        sends command to jog x and y axis using data in GUI for
-        number of setps and pulse width info
-        '''
-        self._update_data()
-        self.jog_xy()
-        
-    def on_jog_x_clicked(self,widget):
-        '''
-        sends command to jog x axis using data in GUI for
-        number of setps and pulse width info
-        '''
-        self._update_data()
-        self.axis_x.jog()
-        
-    def on_jog_y_clicked(self,widget):
-        '''
-        sends command to jog y axis using data in GUI for
-        number of setps and pulse width info
-        '''
-        self._update_data()
-        self.axis_y.jog()
-        
-    def on_jog_z_clicked(self,widget):
-        '''
-        sends command to jog z axis using data in GUI for
-        number of setps and pulse width info
-        '''
-        self._update_data()
-        self.axis_z.jog()
-
-    def on_set_pw_z_clicked(self,widget):
-        '''
-        set z axis pulse width
-        '''
-        self._update_data()
-        self.axis_z.set_pw_info()
-
-    def on_set_pw_y_clicked(self,widget):
-        '''
-        set y axis pulse width
-        '''
-        self._update_data()
-        self.axis_y.set_pw_info()
-
-    def on_set_pw_x_clicked(self,widget):
-        '''
-        set x axis pulse width
-        '''
-        self._update_data()
-        self.axis_x.set_pw_info()
-
-    def on_start_routing_clicked(self, widget):
-        '''
-        will clear the cancel and pause routing bits in firmware
-        
-        if routing is paused it will resume routing
-        this function is called when the transfer gcode button is pressed
-        '''
-        self.start_routing()
-        pass
-#         self.gs_layer_thickness = int(self.layer_thickness.get_text()) 
-#         self.gs_layer_numbers = int(self.layer_numbers.get_text()  ) 
-#         self.set_layer()
-#         for i in range(3):
-#             print 'starting in ',3-i
-#             time.sleep(1)
-#         self.start_routing()
-    
-    def on_erase_coord_clicked(self,widget):
-        '''
-        tell firmware to erase all coordinates in queue
-        '''
-        
-        self.erase_coordinates()
-        
-    def on_cancel_routing_clicked(self, widget):
-        '''
-        tell firmware to stop routing and erase whatever
-        route is planned in the queue
-        '''
-        self.cancel_routing()
-
-    def on_pause_routing_clicked(self, widget):
-        '''
-        tell firmware to pause routing
-            to resume routing send resume command
-        '''
-        self.pause_routing()
-            
-    def on_Transfer_Coord_clicked(self, widget, data = None):
-        '''
-        parse gcode and transmit routing instructions to firmware.
-        
-        sequence:
-        a start_routing command is sent to reset routing pause
-        and routing cancel incase they are set. 
-        
-        a set_feed command is sent to set the feedrate for routing
-        this determines how fast the router will move.
-        
-        parse gcode into coordinates and push data into slow transmission
-        queue so firmware can still accept commands sent to fast
-        transmission queue.
-        
-        '''
-        
-        #do a wait count down before routing
-        for i in xrange(3):
-            print 'starting in:',3-i
-            time.sleep(1)
-            
-        print 'Transfer Coord button activated'
-        self.start_routing()
-        
-        self.gs_feed_cut = int(self.feed_cut.get_text())
-        self.set_feed()
-        print 'scale is ',int(self.gcode_scale.get_text())
-        self.send_coordinates(int(self.gcode_scale.get_text()))
-        #gui_support.send_file(self.GTKGCode_File.get_text())
-        
+        #crawlForward = self.crawlForward.get_active()
+        #crawlReverse = self.crawlReverse.get_active()
+        disableBalancing = self.disableBalancing.get_active()
+        print 'send disable balancing ', disableBalancing
+        # if manual == True:
+        #     self.toggleAutonomous.set_active(False)
+        #     self.toggleSemiAuto.set_active(False)
+      
     def on_rescan_coms_clicked(self,widget, data = None):
         '''
         rescan for available serial ports and update drop down box
@@ -412,23 +242,6 @@ class CrawlerGUI(GuiSupport):
         self.cfg_file_handle.save_config_file()
         gtk.main_quit()
         
-    def _update_data(self):
-        '''
-        get values from GUI objects and apply to gui_support variables
-        '''
-        self.axis_z.dir = self.DirZComboHandle.get_selection_index()
-        self.axis_y.dir = self.DirYComboHandle.get_selection_index()
-        self.axis_x.dir = self.DirXComboHandle.get_selection_index()
-        self.axis_z.step = int(self.StepNumZ.get_text())      
-        self.axis_y.step = int(self.StepNumY.get_text())      
-        self.axis_x.step = int(self.StepNumX.get_text())      
-        self.axis_z.pulse_width_high = int(self.pulsewidth_z_h.get_text())
-        self.axis_z.pulse_width_low = int(self.pulsewidth_z_l.get_text())
-        self.axis_y.pulse_width_high = int(self.pulsewidth_y_h.get_text())
-        self.axis_y.pulse_width_low = int(self.pulsewidth_y_l.get_text())
-        self.axis_x.pulse_width_high = int(self.pulsewidth_x_h.get_text())
-        self.axis_x.pulse_width_low = int(self.pulsewidth_x_l.get_text())
-
     def data_update(self):
         print 'called data update'
         if data_parser.data_frame:
@@ -437,22 +250,9 @@ class CrawlerGUI(GuiSupport):
             self.encoder3.set_text(str(data_parser.data_frame['encoder3']))
             self.encoder4.set_text(str(data_parser.data_frame['encoder4']))
             self.inclineLeftRight.set_value(data_parser.data_frame['roll'])
-        #return 0
 
-        # self.inclineLeftRight.set_value(simulate.accelerometer_x)
-        # self.inclineFrontBack.set_value(simulate.accelerometer_y)
-        # self.movementLeftRight.set_value(simulate.movement_turn)
-        # self.movementReverseForward.set_value(simulate.movement_forward)
         # self.batteryLevel.set_text(str(simulate.battery_level))
-        # self.distanceTraveled.set_text(str(simulate.distance_traveled))
         
-        # #where 4 is the number of magnets embedded inside on the tire hub
-        # self.encoder1.set_text(str(simulate.distance_traveled*4))
-        # self.encoder2.set_text(str(simulate.distance_traveled*4))
-        # self.encoder3.set_text(str(simulate.distance_traveled*4))
-        # self.encoder4.set_text(str(simulate.distance_traveled*4))
-        
-        # #print 'set value to :',simulate.accelerometer_x
         gobject.timeout_add(1000,self.data_update)
 
 if __name__ == "__main__":
